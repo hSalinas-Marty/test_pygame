18cbcf0d5ad70953e2511858c622cff1baff937f
Hugo Salinas--Marty <hugo.salinasmarty@ynov.com>
2025-10-04 09:59:47 +0200

    ajout database

---
+++ Db.py
@@ -0,0 +1,36 @@
+import Game
+import sqlite3
+
+class Database:
+    DB_file = "sauvegardes.db"
+
+    def __init__(self):
+        conn = sqlite3.connect(self.DB_file)
+        c = conn.cursor()
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS parties (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                pseudo TEXT NOT NULL,
+                score INTEGER,
+                niveau INTEGER,
+                date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        ''')
+        conn.commit()
+        conn.close()
+    
+    def sauvegarder_partie(self, pseudo, score, niveau):
+        conn = sqlite3.connect(self.DB_file)
+        c = conn.cursor()
+        c.execute("INSERT INTO parties (pseudo, score, niveau) VALUES (?, ?, ?)",
+                (pseudo, score, niveau))
+        conn.commit()
+        conn.close()
+    
+    def charger_partie(self, pseudo):
+        conn = sqlite3.connect(self.DB_file)
+        c = conn.cursor()
+        c.execute("SELECT score, niveau FROM parties WHERE pseudo=? ORDER BY date DESC LIMIT 1", (pseudo,))
+        row = c.fetchone()
+        conn.close()
+        return row
--- Game.py
+++ Game.py
@@ -1,9 +1,15 @@
-import pygame, sys
+import pygame
 from pygame.locals import *
+
+import Db
 
 class Game:
     def __init__(self, pseudo) :
         self.pseudo = pseudo
+        self.score = 0
+        self.niveau = 1
+        self.data = Db.Database()                                                            # Creation Database
+        self.data.sauvegarder_partie(self.pseudo, self.score, self.niveau)
     
     def run(self, fenetre):
         fenetre.fill("yellow")
@@ -11,3 +17,5 @@ class Game:
         text = font.render(f"Bienvenue, {self.pseudo} !", True, (0, 0, 0))
         fenetre.blit(text, (100, 180))
         pygame.display.flip()
+
+
